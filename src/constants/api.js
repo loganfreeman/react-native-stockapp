374c100b79f19d3231b6bbf4a24b86b6285f7d55
wolonggu <wolonggu@gmail.com>
2018-01-24 18:06:19 -0700

    clean up

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,17 +1,3 @@
 import Config from 'react-native-config';
 
-export const TMDB_URL = Config.TMDB_URL;
-export const TMDB_IMG_URL = Config.TMDB_IMG_URL;
-export const TMDB_API_KEY = Config.TMDB_API_KEY;
-
-export const YOUTUBE_URL = Config.YOUTUBE_URL;
-export const YOUTUBE_API_KEY = Config.YOUTUBE_API_KEY;
-
-export const darkSkyAPIKey = Config.darkSkyAPIKey;
-export const darkSkyAPIURL = Config.darkSkyAPIURL;
-
-export const FISHING_REPORT_URL = "https://wildlife.utah.gov/hotspots/";
-
 export const STOCKING_URL = "https://dwrapps.utah.gov/fishstocking/Fish";
-
-export const IMDB_URL = "http://www.imdb.com/";
